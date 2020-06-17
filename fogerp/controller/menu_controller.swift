4535c54400e7ef957b6f5da3a67ec451cf5e5459
krunal <krunalgadgetguru@gmail.com>
2020-06-17 14:31:04 +0530

    trying to add navigation

---
+++ fogerp/controller/menu_controller.swift
@@ -0,0 +1,17 @@
+//
+//  menu_controller.swift
+//  fogerp
+//
+//  Created by apple on 17/06/20.
+//  Copyright © 2020 kachhua. All rights reserved.
+//
+
+
+import UIKit
+
+class menu_controller:UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ fogerp/utils/protocols.swift
@@ -0,0 +1,11 @@
+//
+//  protocols.swift
+//  fogerp
+//
+//  Created by apple on 17/06/20.
+//  Copyright © 2020 kachhua. All rights reserved.
+//
+
+protocol  HomeControllerDelegate {
+     func HandleMenuToggle()
+}
