a575578649beaad634201570119eebee779c0eb6
krunal <krunalgadgetguru@gmail.com>
2020-07-03 14:19:59 +0530

    log

---
+++ fogerp/SingleRow_LogDetails.swift
@@ -0,0 +1,24 @@
+//
+//  SingleRow_LogDetails.swift
+//  fogerp
+//
+//  Created by apple on 03/07/20.
+//  Copyright © 2020 kachhua. All rights reserved.
+//
+
+import UIKit
+
+class SingleRow_LogDetails: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ fogerp/SwipeController/Lead_Detail_ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  Lead_Detail_ViewController.swift
+//  fogerp
+//
+//  Created by apple on 03/07/20.
+//  Copyright © 2020 kachhua. All rights reserved.
+//
+
+import UIKit
+
+class Lead_Detail_ViewController: UIViewController {
+
+    
+    @IBAction func Backbtn_click(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
